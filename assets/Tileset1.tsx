c47cfd145b2168c7796fca9cea0da939c91c963e
VOIDkuugeki <tri.nguyen210206@vnuk.edu.vn>
2023-06-05 19:44:08 +0700

    Update asset
    Change file's directory

--- assets/Tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Tileset1" tilewidth="16" tileheight="16" tilecount="100" columns="10">
- <image source="../Artwork/assest/8bit/2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png" width="160" height="160"/>
-</tileset>
--- assets/Tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Tileset2" tilewidth="16" tileheight="16" tilecount="512" columns="32">
- <image source="../Artwork/assest/8bit/0x72_16x16DungeonTileset.v5/0x72_16x16DungeonTileset.v5/0x72_16x16DungeonTileset.v5.png" width="512" height="256"/>
-</tileset>
