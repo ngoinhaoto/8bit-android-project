ecb0d3fd5dd7e0330ddbf8c2ad59a589d9edf467
ngoinhaoto <dinhchinhle487@gmail.com>
2023-06-12 17:03:04 +0700

    Revert "update"
    This reverts commit 9fc74f822a8172036c41c55933062d117ae44bac.

--- core/src/com/voider/game/Character.java
+++ core/src/com/voider/game/Character.java
@@ -1,49 +1,66 @@
 package com.voider.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class Character extends Sprite {
-    public enum State { IDLING, WALKING_RIGHT, WALKING_LEFT};
+    private static final float FRAME_TIME = 0.2f;
+    public enum State { IDLING, WALKING_RIGHT, WALKING_LEFT }
     public State currentState;
     public State previousState;
-    public Texture characterTexture;
+    public TextureAtlas textureAtlas;
     public Vector2 position;
-    private Animation<Texture> charIdle;
-    private Animation<Texture> charWalk;
-    private float stateTimer;
+    private Animation<TextureRegion> charIdle;
+    private Animation<TextureRegion> charWalk;
+    private float stateTime;
     public float speed;
+    private boolean isLeft = false;
+
     public Character() {
-        this.characterTexture = new Texture("char/idle/char32x32 idle 1.png");
+        textureAtlas = new TextureAtlas(Gdx.files.internal("char/character.atlas"));
         this.position = new Vector2();
         this.speed = 100f;
 
         currentState = State.IDLING;
         previousState = State.IDLING;
 
-        Array<Texture> frame1 = new Array<Texture>();
+        charIdle = new Animation<TextureRegion>(FRAME_TIME, textureAtlas.findRegions("idle"));
+        charWalk = new Animation<TextureRegion>(FRAME_TIME, textureAtlas.findRegions("walk"));
 
+<<<<<<< HEAD
         //Walk Animation
 //        for (int i = 1; i < 4; i++) {
 //            frame1.add(new TextureRegion(new Texture("char/idle/idle_spritesheet.png"), i*32, 32,32, 32));
 //        }
-
         frame1.add(new Texture("char/walk/char32x32 walk1.png"));
-        frame1.add(new Texture("char/walk/char32x32 walk2.png"));
         frame1.add(new Texture("char/walk/char32x32 walk3.png"));
-        charWalk = new Animation<Texture>(0.4f, frame1);
+        frame1.add(new Texture("char/walk/char32x32 walk2.png"));
+        charWalk = new Animation<Texture>(0.1f, frame1);
 
         Array<Texture> frame2 = new Array<Texture>();
         //Idle Animation
         frame2.add(new Texture("char/idle/char32x32 idle 1.png"));
         frame2.add(new Texture("char/idle/char32x32 idle 2.png"));
+<<<<<<< HEAD
+        charIdle = new Animation<Texture>(0.1f, frame2);
+=======
         charIdle = new Animation<Texture>(0.4f, frame2);
+=======
+        charIdle.setFrameDuration(FRAME_TIME);
+        charWalk.setFrameDuration(FRAME_TIME);
+>>>>>>> 76d9fb74ec5139dbc4a48e6ae0f3c3cef223a245
+>>>>>>> parent of 361f1a8 (Revert "update")
     }
+
     public void update(float delta, float joystickX, float joystickY) {
         // Update the character's position based on input or game logic
         float deltaX = joystickX * speed * delta;
@@ -51,27 +68,28 @@ public class Character extends Sprite {
         position.x += deltaX;
         position.y += deltaY;
 
-        this.characterTexture = getFrame(delta);
+        // Increment the stateTime for animation
+        stateTime += delta;
     }
 
-    public Texture getFrame(float deltaTime) {
+    public TextureRegion getFrame(float deltaTime) {
         currentState = getState();
-        Texture region;
-
+        TextureRegion region;
         switch (currentState) {
             case IDLING:
-                region = charIdle.getKeyFrame(stateTimer);
+                region = charIdle.getKeyFrame(stateTime, true);
+                break;
             case WALKING_LEFT:
-                region = charWalk.getKeyFrame(stateTimer, true);
+                region = charWalk.getKeyFrame(stateTime, true);
+                this.isLeft = true;
                 break;
             case WALKING_RIGHT:
-                region = charWalk.getKeyFrame(stateTimer, true);
+                region = charWalk.getKeyFrame(stateTime, true);
+                this.isLeft = false;
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + currentState);
         }
-        stateTimer = currentState == previousState ? stateTimer + deltaTime : 0;
-        previousState = currentState;
         return region;
     }
 
@@ -79,7 +97,7 @@ public class Character extends Sprite {
         return this.currentState;
     }
 
-    //Set State of Character
+    // Set State of Character
     public void setState(String stt) {
         // Handle state change
         switch (stt) {
@@ -103,15 +121,22 @@ public class Character extends Sprite {
         }
     }
 
-    public void render(SpriteBatch spriteBatch, boolean flip) {
+    public void render(SpriteBatch spriteBatch) {
+        TextureRegion currentFrame = getFrame(Gdx.graphics.getDeltaTime());
         // Render the character at its current position
-        if (flip) {
-            spriteBatch.draw(this.characterTexture, position.x + this.characterTexture.getWidth(), position.y,
-                    -this.characterTexture.getWidth(), this.characterTexture.getHeight());
+        float textureWidth = 32; // Set the desired texture width
+        float textureHeight = 32; // Set the desired texture height
+        if (isLeft) {
+            spriteBatch.draw(currentFrame, position.x + textureWidth, position.y,
+                    -textureWidth, textureHeight);
+            Gdx.app.log("Txt", String.valueOf(textureWidth));
         } else {
-            spriteBatch.draw(this.characterTexture, position.x, position.y);
+            spriteBatch.draw(currentFrame, position.x, position.y, textureWidth, textureHeight);
+            Gdx.app.log("Txt", String.valueOf(textureWidth));
         }
     }
+
+
 
     public Vector2 getPosition() {
         return position;
@@ -124,5 +149,4 @@ public class Character extends Sprite {
     public void dispose() {
 
     }
-
 }
--- core/src/com/voider/game/Screen/FirstLevelScreen.java
+++ core/src/com/voider/game/Screen/FirstLevelScreen.java
@@ -21,7 +21,7 @@ public class FirstLevelScreen implements Screen {
     private float initialCameraX = 470; // Adjust this value to set the initial x-coordinate of the camera
     private float initialCameraY = 935; // Adjust this value to set the initial y-coordinate of the camera
     private OrthographicCamera gameCam;
-//    private HUD hud;
+    //    private HUD hud;
     private SpriteBatch batch;
 
     private TiledMap tiledMap;
@@ -43,9 +43,9 @@ public class FirstLevelScreen implements Screen {
         loadCharacter();
         loadJoystick();
     }
-     @Override
+    @Override
     public void show() {
-         // Initialize resources or perform any setup
+        // Initialize resources or perform any setup
     }
 
     public void loadCamera() {
@@ -92,14 +92,37 @@ public class FirstLevelScreen implements Screen {
             public void changed(ChangeEvent event, Actor actor) {
                 // Get the joystick position
                 float knobPercentX = touchpad.getKnobPercentX();
-
+                float knobPercentY = touchpad.getKnobPercentY();
                 // Set the state of the character based on the joystick position
+<<<<<<< HEAD
+<<<<<<< HEAD
+                if (knobPercentX > 0.1f) {
+                    // Move character right
+                    character.setState("RIGHT");
+                } else if (knobPercentX < -0.1f) {
+=======
+=======
+>>>>>>> parent of 361f1a8 (Revert "update")
                 if (knobPercentX > 0.2f) {
                     // Move character right
                     character.setState("RIGHT");
                 } else if (knobPercentX < -0.2f) {
+>>>>>>> parent of 76d9fb7 (Update animation (Operate success))
                     // Move character left
                     character.setState("LEFT");
+=======
+                if (knobPercentY > 0f || knobPercentY < 0f) {
+                    if (knobPercentX > 0f) {
+                        // Move character right
+                        character.setState("RIGHT");
+                    } else if (knobPercentX < -0f) {
+                        // Move character left
+                        character.setState("LEFT");
+                    } else {
+                        // Idle state
+                        character.setState("IDLE");
+                    }
+>>>>>>> 76d9fb74ec5139dbc4a48e6ae0f3c3cef223a245
                 } else {
                     // Idle state
                     character.setState("IDLE");
@@ -115,36 +138,36 @@ public class FirstLevelScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Update the character's position and state based on the joystick input
-        float joystickX = touchpad.getKnobPercentX();
-        float joystickY = touchpad.getKnobPercentY();
-        character.update(delta, joystickX, joystickY);
-
-        // Update the camera's position to center on the character
-        float cameraX = character.getPosition().x; // Adjust this if necessary
-        float cameraY = character.getPosition().y; // Adjust this if necessary
-        gameCam.position.set(cameraX, cameraY, 0);
-        gameCam.update();
-
-        // Render the tile map
+        // Update and render the tile map
         mapRenderer.setView(gameCam);
         mapRenderer.render();
+        // Other rendering logic
 
-        // Render the character
+        // Rendering character
         batch.setProjectionMatrix(gameCam.combined);
         batch.begin();
+<<<<<<< HEAD
+        if(character.getState() == Character.State.WALKING_LEFT) {
+=======
+<<<<<<< HEAD
         if (character.getState() == Character.State.WALKING_LEFT) {
+>>>>>>> parent of 361f1a8 (Revert "update")
             character.render(batch, true);
         } else {
             character.render(batch, false);
         }
+=======
+        character.render(batch);
+>>>>>>> 76d9fb74ec5139dbc4a48e6ae0f3c3cef223a245
         batch.end();
 
-        // Update and render the joystick
+        float joystickX = touchpad.getKnobPercentX();
+        float joystickY = touchpad.getKnobPercentY();
+        character.update(delta, joystickX, joystickY);
+
         stage.act(delta);
         stage.draw();
     }
-
 
     @Override
     public void resize(int width, int height) {
