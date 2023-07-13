1c01dda0e40d94b893864f742562d76a9f146594
ngoinhaoto <dinhchinhle487@gmail.com>
2023-07-13 20:33:54 +0700

    update level2 map

---
+++ assets/map/dungeon1/DarkDungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="DarkDungeon" tilewidth="16" tileheight="16" tilecount="144" columns="9">
+ <image source="../../../../../../Desktop/DarkDungeon.png" width="144" height="256"/>
+</tileset>
---
+++ assets/map/dungeon1/tileset x2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset x2" tilewidth="16" tileheight="16" tilecount="13616" columns="148">
+ <image source="../../../../../../Desktop/Tech Dungeon Roguelite - Asset Pack (DEMO)/tileset x2.png" width="2368" height="1472"/>
+</tileset>
